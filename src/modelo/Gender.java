3b969e8d50e7a7ee94ec8769da493ebc822434cd
wild.chamo <joselitobedoya230@gmail.com>
2021-05-24 14:23:41 -0500

    agrego el modelo del genero

--- src/modelo/Gender.java
+++ src/modelo/Gender.java
@@ -10,5 +10,39 @@ package modelo;
  * @author wild.chamo
  */
 public class Gender {
+    private int idGender;
+    private String nameG;
+
+    public Gender() {
+    }
+
+    public Gender(int idGender, String nameG) {
+        this.idGender = idGender;
+        this.nameG = nameG;
+    }
+
+    public Gender(String nameG) {
+        this.nameG = nameG;
+    }
+
+    public int getIdGender() {
+        return idGender;
+    }
+
+    public void setIdGender(int idGender) {
+        this.idGender = idGender;
+    }
+
+    public String getNameG() {
+        return nameG;
+    }
+
+    public void setNameG(String nameG) {
+        this.nameG = nameG;
+    }
     
+          @Override
+    public String toString() {
+        return "Gender{" + "idGender=" + idGender + ", nameG=" + nameG + '}';
+    }
 }
