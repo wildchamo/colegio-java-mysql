c4546956f0f486f9a35e261725d998404c9ea767
wild.chamo <joselitobedoya230@gmail.com>
2021-05-24 14:28:05 -0500

    creacion del controlador de city para lograr una inserción

---
+++ src/controlador/ControladorCity.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import modelo.BaseDatos;
+import modelo.City;
+/**
+ *
+ * @author wild.chamo
+ */
+public class ControladorCity {
+    
+    public boolean insertCity(City objc) {
+        boolean t=false;
+        String sql="insert into cities(nombreCity) value('"+objc.getNameC()+"');";
+        BaseDatos objbd=new BaseDatos();
+        t=objbd.ejecutarSQL(sql);
+        return t;
+        
+        
+    }
+}
