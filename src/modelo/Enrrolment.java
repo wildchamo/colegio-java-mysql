57d841366b4991701e5fbe979fb282f1d729a635
wild.chamo <joselitobedoya230@gmail.com>
2021-05-24 15:16:42 -0500

    modelo enrrolment

---
+++ src/modelo/Enrrolment.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author wild.chamo
+ */
+public class Enrrolment {
+
+    private int enrrolmentID;
+    private int courseIDfk;
+    private int studentIDfk;
+    
+}
