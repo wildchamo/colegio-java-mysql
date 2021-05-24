b0016943af2d42d7346e6afc4c31901cf3ba1039
wild.chamo <joselitobedoya230@gmail.com>
2021-05-24 15:04:57 -0500

    creación del modelo de course

---
+++ src/modelo/course.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author wild.chamo
+ */
+public class course {
+    private int courseID;
+    private String nameC;
+    private String classroom;
+    private String imgCourse;
+
+    public course() {
+    }
+
+    public course(String nameC, String classroom, String imgCourse) {
+        this.nameC = nameC;
+        this.classroom = classroom;
+        this.imgCourse = imgCourse;
+    }
+
+    public course(int courseID, String nameC, String classroom, String imgCourse) {
+        this.courseID = courseID;
+        this.nameC = nameC;
+        this.classroom = classroom;
+        this.imgCourse = imgCourse;
+    }
+
+    public int getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(int courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getNameC() {
+        return nameC;
+    }
+
+    public void setNameC(String nameC) {
+        this.nameC = nameC;
+    }
+
+    public String getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(String classroom) {
+        this.classroom = classroom;
+    }
+
+    public String getImgCourse() {
+        return imgCourse;
+    }
+
+    public void setImgCourse(String imgCourse) {
+        this.imgCourse = imgCourse;
+    }
+
+    @Override
+    public String toString() {
+        return "course{" + "courseID=" + courseID + ", nameC=" + nameC + ", classroom=" + classroom + ", imgCourse=" + imgCourse + '}';
+    }
+    
+    
+    
+    
+    
+}
