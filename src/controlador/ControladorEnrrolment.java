9ef53f4d3a1d034a54b51e0d56f3d2281c32a779
wild.chamo <joselitobedoya230@gmail.com>
2021-05-26 13:01:11 -0500

    controlador enrrolment

--- src/controlador/ControladorEnrrolment.java
+++ src/controlador/ControladorEnrrolment.java
@@ -6,10 +6,19 @@
 package controlador;
 import modelo.Enrrolment;
 import modelo.BaseDatos;
+import modelo.Course;
+import modelo.Student;
 /**
  *
  * @author wild.chamo
  */
 public class ControladorEnrrolment {
-    
+        public boolean asignarCurso(Enrrolment matricula,Student estudiante, Course curso){
+        boolean t = false;
+        String sql = "INSERT INTO enrrolments (studyDay,studentIDfk,courseIDfk ) VALUE(" + matricula.getStudyDay() + "," + estudiante.getStudentID() + "," + curso.getCourseID() + ")";
+        BaseDatos objbd = new BaseDatos();
+        t = objbd.ejecutarSQL(sql);
+        
+        return t;
+    }
 }
