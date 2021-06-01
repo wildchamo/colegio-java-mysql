e427ecbabe312eca5724de41592ffeba33dc98a0
wild.chamo <joselitobedoya230@gmail.com>
2021-06-01 10:32:16 -0500

    creación del metodo para llamar los estudiantes según el curso en el controlador

--- src/controlador/ControladorStudent.java
+++ src/controlador/ControladorStudent.java
@@ -4,37 +4,39 @@
  * and open the template in the editor.
  */
 package controlador;
+
 import java.util.LinkedList;
 import modelo.BaseDatos;
 import modelo.Student;
+
 /**
  *
  * @author wild.chamo
  */
 public class ControladorStudent {
-       public boolean insertStudent(Student objs) {
-        boolean t=false;
-        String sql="insert into students(name1S,name2S,surname1S,surname2S,mobileS,adressS,stratumS,institutionalEmailS,custodianMobile1,custodianMobile2,userS,passwordS,idCitiesfk,idGenderfk) values('"+objs.getName1S()+"','"+objs.getName2S()+"','"+objs.getSuname1S()+"','"+objs.getSuname2S()+"','"+objs.getMobileS()+"','"+objs.getAddressS()+"','"+objs.getStratumS()+"','"+objs.getInstitutionalEmailS()+"','"+objs.getCustodianMobile1()+"','"+objs.getCustodianMobile2()+"','"+objs.getUserS()+"','"+objs.getPasswordS()+"','"+objs.getIdCityfk()+"','"+objs.getIdGenderfk()+"');";
-        BaseDatos objbd=new BaseDatos();
-        t=objbd.ejecutarSQL(sql);
-        return t;              
+
+    public boolean insertStudent(Student objs) {
+        boolean t = false;
+        String sql = "insert into students(name1S,name2S,surname1S,surname2S,mobileS,adressS,stratumS,institutionalEmailS,custodianMobile1,custodianMobile2,userS,passwordS,idCitiesfk,idGenderfk) values('" + objs.getName1S() + "','" + objs.getName2S() + "','" + objs.getSuname1S() + "','" + objs.getSuname2S() + "','" + objs.getMobileS() + "','" + objs.getAddressS() + "','" + objs.getStratumS() + "','" + objs.getInstitutionalEmailS() + "','" + objs.getCustodianMobile1() + "','" + objs.getCustodianMobile2() + "','" + objs.getUserS() + "','" + objs.getPasswordS() + "','" + objs.getIdCityfk() + "','" + objs.getIdGenderfk() + "');";
+        BaseDatos objbd = new BaseDatos();
+        t = objbd.ejecutarSQL(sql);
+        return t;
     }
 
     public LinkedList<Student> consultarEstudiantes() {
-           LinkedList<Student> ls=null;
-        String sql="select * from students;";
-        Student objc=new Student();
-        ls=objc.consultarEstudiantes(sql);
+        LinkedList<Student> ls = null;
+        String sql = "select * from students;";
+        Student objc = new Student();
+        ls = objc.consultarEstudiantes(sql);
         return ls;
     }
-    
-     /*   public LinkedList<Student> consultarEstudiantesPorAsignatura() {
-        LinkedList<Student> ls=null;
-        String sql="select * from students;";
-        Student objc=new Student();
-        ls=objc.consultarEstudiantes(sql);
+
+    public LinkedList<Student> consultarEstudiantesPorCurso(int idCurso) {
+        LinkedList<Student> ls = null;
+        String sql = "SELECT *  FROM STUDENTS s, enrollments e WHERE s.studentID=e.idStudentfk and e.idCoursefk=" + idCurso + ";";
+        Student objc = new Student();
+        ls = objc.consultarEstudiantes(sql);
         return ls;
     }
-    */
-    
+
 }
