63367e268dcb34f098a35ffe50369de676f82372
wild.chamo <joselitobedoya230@gmail.com>
2021-06-01 11:05:04 -0500

    creación del metodo para llamar un curso por el id

--- src/controlador/ControladorCourse.java
+++ src/controlador/ControladorCourse.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package controlador;
+
 import java.util.LinkedList;
 import modelo.BaseDatos;
 import modelo.Course;
@@ -13,21 +14,31 @@ import modelo.Course;
  * @author wild.chamo
  */
 public class ControladorCourse {
-        public boolean insertCourse(Course objc) {
-        boolean t=false;
-        String sql="insert into courses(nameCourse,classroom,imgCourse) value(?,?,?);";
-        Course objbc=new Course();
-        t=objbc.insertCourse(sql,objc);
+
+    public boolean insertCourse(Course objc) {
+        boolean t = false;
+        String sql = "insert into courses(nameCourse,classroom,imgCourse) value(?,?,?);";
+        Course objbc = new Course();
+        t = objbc.insertCourse(sql, objc);
         return t;
-        } 
-        
-        public LinkedList<Course> consultarCursos() {
-        LinkedList<Course> lc=null;
-        String sql="select * from courses;";
-        Course objco=new Course();
-        lc=objco.consultarCursos(sql);
-        
+    }
+
+    public LinkedList<Course> consultarCursos() {
+        LinkedList<Course> lc = null;
+        String sql = "select * from courses;";
+        Course objco = new Course();
+        lc = objco.consultarCursos(sql);
+
         return lc;
-    }   
-    
+    }
+
+    public LinkedList<Course> consultarCursoPorID(int cursoID) {
+        LinkedList<Course> lc = null;
+        String sql = "select * from courses WHERE courseID=" + cursoID + ";";
+        Course objco = new Course();
+        lc = objco.consultarCursos(sql);
+
+        return lc;
+    }
+
 }
--- src/controlador/ControladorSubject.java
+++ src/controlador/ControladorSubject.java
@@ -32,6 +32,9 @@ public class ControladorSubject {
         return ls;
     }
     
+    
+    
+    
 }
 
 
