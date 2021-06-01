2b8f5c5e477853d5ab66a29ca4590c46682c8554
wild.chamo <joselitobedoya230@gmail.com>
2021-05-31 19:11:46 -0500

    creación del metodo para realizar la inserción de scores

--- src/controlador/ControladorScore.java
+++ src/controlador/ControladorScore.java
@@ -11,5 +11,11 @@ import modelo.BaseDatos;
  * @author wild.chamo
  */
 public class ControladorScore {
-    
+       public boolean insertScore(Score objs) {
+        boolean t = false;
+        String sql = "{call insertScore('" + objs.getScore1()+ "')}";
+        BaseDatos objbd = new BaseDatos();
+        t = objbd.ejecutarSQL(sql);
+        return t;
+    }
 }
